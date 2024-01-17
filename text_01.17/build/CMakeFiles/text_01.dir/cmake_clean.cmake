b1d119ee57f910e90764a3504890bc7dd1448f46
Louis_Code <118169771+LuomingyuCode@users.noreply.github.com>
2024-01-17 23:46:13 +0800

    Add files via upload
    利用Cmake调试多文件

---
+++ text_01.17/build/CMakeFiles/text_01.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_01.dir/main.c.obj"
+  "CMakeFiles/text_01.dir/main.c.obj.d"
+  "CMakeFiles/text_01.dir/max.c.obj"
+  "CMakeFiles/text_01.dir/max.c.obj.d"
+  "libtext_01.dll.a"
+  "text_01.exe"
+  "text_01.exe.manifest"
+  "text_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/text_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ text_01.17/main.c
@@ -0,0 +1,10 @@
+#include "max.h"
+
+int main()
+{
+    // int a = 10, b = 10;
+    int a = 11, b = 10;
+    int c = max(a, b);
+    printf("%d\n", c);
+    return 0;
+}
